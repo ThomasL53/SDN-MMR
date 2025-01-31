61f603cc46082307391706b4a4f98b35cc8819cd
Yann-G-29 <y.gourlet@orange.fr>
2025-01-31 12:48:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,6 +59,8 @@ echo "Réupération de l'image docker pour le client Firefox  ..."
 
 sudo docker pull thomasl53/firefox >> "$LOG_FILE" 2>&1
 
+cd ..
+
 echo " Installation de Grafana ..."
 
 sudo apt-get install -y apt-transport-https software-properties-common wget >> "$LOG_FILE" 2>&1
@@ -69,16 +71,20 @@ sudo apt-get update >> "$LOG_FILE" 2>&1
 
 sudo apt-get install grafana >> "$LOG_FILE" 2>&1
 
+sudo rm prometheus-3.1.0.linux-386.tar.gz >> "$LOG_FILE" 2>&1
+
 echo " Installation de Prometheus ..."
 
 wget https://github.com/prometheus/prometheus/releases/download/v3.1.0/prometheus-3.1.0.linux-386.tar.gz >> "$LOG_FILE" 2>&1
 
-tar -xzf prometheus-*.tar.gz
+tar -xzf prometheus-*.tar.gz >> "$LOG_FILE" 2>&1
 
-cp prometheus.yml prometheus-3.1.0.linux-amd64/prometheus.yml
+cp prometheus.yml prometheus-3.1.0.linux-amd64/prometheus.yml >> "$LOG_FILE" 2>&1
 
 echo " Installation de Node_Exporter ..."
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz >> "$LOG_FILE" 2>&1
 
-tar -xzf node_*.tar.gz
+tar -xzf node_*.tar.gz >> "$LOG_FILE" 2>&1
+
+sudo rm node_exporter-1.8.2.linux-amd64.tar.gz >> "$LOG_FILE" 2>&1
